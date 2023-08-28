0123f3a3d5905b6c4127474916fdc9c35fbd8d8d
Viktor <novikov.vd@phystech.edu>
2023-08-28 19:24:43 +0300

    added my_strlen function

--- my_strings.h
+++ my_strings.h
@@ -1,7 +1,8 @@
 #ifndef MY_STRINGS_H
 #define MY_STRINGS_H
 
-void  my_puts   (const char *buf);
-char *my_strchr (const char *str, int ch);
+void   my_puts   (const char *buf);
+char  *my_strchr (const char *str, int ch);
+size_t my_strlen (const char *str);
 
 #endif /*MY_STRINGS_H*/
---
+++ my_strlen.cpp
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "my_strings.h"
+
+size_t my_strlen(const char *str)
+{
+    size_t n_count = 0;
+    while (*(str + n_count) != '\0')
+    n_count++;
+
+    return n_count;
+}
