e3304be95270191115e36f4639cf8bccc807acdb
Viktor <novikov.vd@phystech.edu>
2023-08-29 20:20:24 +0300

    codestyle

--- my_strdup.cpp
+++ my_strdup.cpp
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include "my_strings.h"
 
-char *my_strdup(const char *str)
+char *my_strdup (const char *str)
 {
     size_t index = 0;
 
-    while(*(str + index) != '\0')
+    while (*(str + index) != '\0')
         index++;
 
     char *copy_str = (char *)malloc(index + 1);
 
-    for(int i = 0;i < index + 1; i++)
+    for (int i = 0;i < index + 1; i++)
         copy_str[i] = str[i];
 
     copy_str[index + 1] = '\0';
